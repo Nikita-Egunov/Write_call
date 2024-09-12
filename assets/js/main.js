ee6361364aa9f1586bc9d6b0e8e5f7782883c080
Nikita-Egunov <negunov1003@gmail.com>
2024-09-12 07:56:09 +0300

    + swiper (не рабочий)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,18 @@
+import Swiper from "swiper";
+
+import { Pagination, Navigation } from "swiper/modules";
+
+var firstSwiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    pagination: {
+        el: ".swiper-pagination",
+      },
+  });
+
 function performSearch() {
     const searchQuery = document.getElementById('search-input').value;
     if (searchQuery.trim() !== "") {
