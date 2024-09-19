84292847427262c0e9d37977d182d74b52715c60
Nikita-Egunov <negunov1003@gmail.com>
2024-09-19 21:54:39 +0300

    Скелет футера

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,8 +1,22 @@
-var thirdSwiper = new Swiper(".thirdSwiper", {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-  },
-})
+let x = 0;
+
+while (true) {
+  let a = 2 * x + 1;
+  let b = x - 3;
+  
+  if (b === 0) {
+    console.log("Деление на ноль невозможно");
+    break;
+  }
+  
+  let c = a / b;
+  
+  if (c === 4) {
+    console.log(`x = ${x}`);
+    break;
+  }
+  
+  x++;
+}
+
+
