fee8709a0a0873bbcbda1a259fc515bd0cf897d3
Nikita-Egunov <negunov1003@gmail.com>
2024-10-05 17:04:44 +0300

    Начал делать адаптив

--- assets/js/test.js
+++ assets/js/test.js
@@ -1,2 +1,10 @@
+const burgerButton = document.querySelector('.burger-button');
+    const menus = document.querySelectorAll('.menu');
 
+    burgerButton.addEventListener('click', () => {
+      burgerButton.classList.toggle('active');
+      menus.forEach(menu => {
+        menu.classList.toggle('active');
+      });
+    });
 
