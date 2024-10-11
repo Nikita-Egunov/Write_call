5685a176e2a7c1c7d9962211b76a7370a5c0d886
Никита-Егунов <negunov1003@gmail.com>
2024-10-11 18:54:14 +0300

    Оно адаптивно

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,111 +1,155 @@
-// import { setupModal } from './modal.js'
+import Swiper from '../../node_modules/swiper/swiper-bundle.min.mjs';
+import { Navigation, Pagination } from '../../node_modules/swiper/modules/index.min.mjs';
+import { setupModal } from './modal.js';
 
+var firstSwiper = new Swiper(".firstSwiper", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
 
-// var firstSwiper = new Swiper(".firstSwiper", {
-//     navigation: {
-//         nextEl: ".swiper-button-next",
-//         prevEl: ".swiper-button-prev",
-//     },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
 
-//     pagination: {
-//         el: ".swiper-pagination",
-//         clickable: true,
-//     },
-// });
+var secondSwiper = new Swiper(".secondSwiper", {
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
 
-// var secondSwiper = new Swiper(".secondSwiper", {
-//     pagination: {
-//         el: ".swiper-pagination",
-//         clickable: true,
-//     },
-// });
+var thirdSwiper = new Swiper(".thirdSwiper", {
+    slidesPerView: 2,
+    spaceBetween: 10,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    },    
+    breakpoints: {
+        1700: {
+            slidesPerView: 5,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: false
+            }
+        },
+    }
+});
 
-// var thirdSwiper = new Swiper(".thirdSwiper", {
-//     slidesPerView: 1,
-//     spaceBetween: 30,
-//     pagination: {
-//         el: ".swiper-pagination",
-//         clickable: true,
-//     },
-//     breakpoints: {
-//         1700: {
-//             slidesPerView: 3,
-//         },
-//         700: {
-//             slidesPerView: 2,
-//         },
-//     }
-// });
+var fourthSwiper = new Swiper(".fourtSwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        1700: {
+            slidesPerView: 3,
+        },
+        700: {
+            slidesPerView: 2,
+        },
+    }
+});
 
 
 // setupModal('my-modal', 'open-modal-btn', 'close-my-modal-btn');
 
 // setupModal('second-modal', 'open-second-modal-btn', 'close-second-modal-btn');
 
-// function catalogToggle(id) {
-//     document.getElementById(id).addEventListener('click', () => {
-//         document.getElementById('catalog-modal').classList.toggle('catalog-open')
-//         document.getElementById('links-open-btn-1').classList.add('active')
-//         document.getElementById('catalog__list-1').classList.add('catalog-open')
-//     })
-// };
 
-// function openLinks(buttonId, listId) {
-//     const button = document.getElementById(buttonId);
-//     const list = document.getElementById(listId);
+function catalogToggle(id) {
+    const windowWidth = document.documentElement.clientWidth;
+    
+    document.getElementById(id).addEventListener('click', () => {
+        document.getElementById('catalog-modal').classList.toggle('catalog-open')
+        if (windowWidth > 1450) {
+            document.querySelectorAll('.catalog__item, .catalog__list').forEach(el => {
+                el.classList.remove('active', 'catalog-open');
+            });
+        document.getElementById('links-open-btn-1').classList.add('active')
+        document.getElementById('catalog__list-1--full').classList.add('catalog-open')
+        } else {
+            document.querySelectorAll('.catalog__item, .catalog__list').forEach(el => {
+                el.classList.remove('active', 'catalog-open');
+            });
+            document.getElementById('links-open-btn-1').classList.add('active')
+            document.getElementById('catalog__list-1').classList.add('catalog-open')
+        }
+    })
+};
 
-//     button.addEventListener('click', () => {
-//         const isActive = button.classList.contains('active');
+function openLinks(buttonId, listId, listIdFull) {
+    const button = document.getElementById(buttonId);
+    const listFull = document.getElementById(listIdFull);
+    const list = document.getElementById(listId)
 
-//         // Удаляем класс 'active' у всех кнопок
-//         document.querySelectorAll('.catalog__item').forEach(btn => {
-//             btn.classList.remove('active');
-//         });
+    button.addEventListener('click', () => {
+        const isActive = button.classList.contains('active');
 
-//         // Удаляем класс 'catalog-open' у всех списков
-//         document.querySelectorAll('.catalog__list').forEach(item => {
-//             item.classList.remove('catalog-open');
-//         });
-
-//         // Если кнопка не активна, добавляем классы
-//         if (!isActive) {
-//             button.classList.add('active');
-//             list.classList.add('catalog-open');
-//         }
-//     });
-// }
-
-// // Вызов функции для каждой пары кнопка-список
-// const buttonListPairs = [
-//     ['links-open-btn-1', 'catalog__list-1'],
-//     ['links-open-btn-2', 'catalog__list-2'],
-//     ['links-open-btn-3', 'catalog__list-3'],
-//     ['links-open-btn-4', 'catalog__list-4'],
-//     ['links-open-btn-5', 'catalog__list-5'],
-//     ['links-open-btn-6', 'catalog__list-6'],
-//     ['links-open-btn-7', 'catalog__list-7'],
-//     ['links-open-btn-8', 'catalog__list-8'],
-//     ['links-open-btn-9', 'catalog__list-9'],
-//     ['links-open-btn-10', 'catalog__list-10']
-// ];
-
-// buttonListPairs.forEach(([buttonId, listId]) => {
-//     openLinks(buttonId, listId);
-// });
-
-// catalogToggle('open-catalog-modal-btn')
-// catalogToggle('close-catalog-btn')
-
-const burgerButton = document.querySelector('.burger-button');
-    const menus = document.querySelectorAll('.menu');
-
-    burgerButton.addEventListener('click', () => {
-      burgerButton.classList.toggle('cliked');
-      menus.forEach(menu => {
-        menu.classList.toggle('visible');
-      });
+        document.querySelectorAll('.catalog__item, .catalog__list').forEach(el => {
+            el.classList.remove('active', 'catalog-open');
+        });
+        const windowWidth = document.documentElement.clientWidth;
+        if (windowWidth > 1450) {
+            if (!isActive) {
+                button.classList.add('active');
+                listFull.classList.add('catalog-open');
+            } else {
+                button.classList.add('active');
+                listFull.classList.add('catalog-open');
+            } 
+        } else {
+            if (!isActive) {
+                button.classList.add('active');
+                list.classList.add('catalog-open');
+            } else {
+                button.classList.add('active');
+                list.classList.add('catalog-open');
+            }
+        }
     });
+}
 
+const buttonListPairs = [
+    ['links-open-btn-1', 'catalog__list-1','catalog__list-1--full'],
+    ['links-open-btn-2', 'catalog__list-2', 'catalog__list-2--full'],
+    ['links-open-btn-3', 'catalog__list-3', 'catalog__list-3--full'],
+    ['links-open-btn-4', 'catalog__list-4', 'catalog__list-4--full'],
+    ['links-open-btn-5', 'catalog__list-5', 'catalog__list-5--full'],
+    ['links-open-btn-6', 'catalog__list-6', 'catalog__list-6--full'],
+    ['links-open-btn-7', 'catalog__list-7', 'catalog__list-7--full'],
+    ['links-open-btn-8', 'catalog__list-8', 'catalog__list-8--full'],
+    ['links-open-btn-9', 'catalog__list-9', 'catalog__list-9--full'],
+    ['links-open-btn-10', 'catalog__list-10', 'catalog__list-10--full']
+];
 
+buttonListPairs.forEach(([buttonId, listId, listIdFull]) => {
+    openLinks(buttonId, listId, listIdFull);
+});
+
+catalogToggle('open-catalog-modal-btn')
+catalogToggle('close-catalog-btn')
+
+const burgerButton = document.getElementById('burger-button');
+const charm = document.getElementById('charm');
+const menus = document.querySelectorAll('.menu');
+
+burgerButton.addEventListener('click', () => {
+    document.querySelector('.conteiner__header').classList.toggle('unPadding')
+    menus.forEach(menu => {
+        menu.classList.toggle('visible');
+    });
+});
+
+charm.addEventListener('click', () => {
+    menus.forEach(menu => {
+        menu.classList.toggle('visible');
+    });
+});
 
 console.log('js is here');
